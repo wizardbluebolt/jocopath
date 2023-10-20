c4800f9b5d445e6cbe9e3bca61e36d6138de8024
Matt Rosen <wizardbluebolt@gmail.com>
2023-10-20 08:00:32 -0700

    Structured Homeless and Services pages.

--- path-web/src/components/Homeless.vue
+++ path-web/src/components/Homeless.vue
@@ -1,16 +1,61 @@
 <template>
-    <v-container class="fill-height">
-      <v-responsive class="align-center text-center fill-height">
-  
-        <v-row class="d-flex align-center justify-center">
-          <v-col cols="auto">
-            Homelessness in Josephine County
-          </v-col>
-        </v-row>
-      </v-responsive>
-    </v-container>
+    <v-tabs
+      v-model="viewTab"
+      fixed-tabs
+      bg-color="primary"
+    >
+      <v-tab value="facts">Facts</v-tab>
+      <v-tab value="problem">The Problem</v-tab>
+      <v-tab value="services">Types of Services</v-tab>
+      <v-tab value="grantspass">City of Grants Pass</v-tab>
+    </v-tabs>
+    <v-card-text>
+      <v-window v-model="viewTab">
+        <v-window-item value="facts">
+          <v-card>
+            <v-card-text>
+              <h3 class="pa-2">Facts about homelessness in Josephine County</h3>
+              <p class="pa-4">As of 2023, there are estimated to be over 600 homeless people in Grants Pass alone
+                among its 38,000 residents.</p>
+              <p class="pa-4">The median monthly rental in Grants Pass is $x,xxx while the median income is $x,xxx.</p>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+        <v-window-item value="problem">
+          <v-card>
+            <v-card-text>
+              <h3 class="pa-2">Complexity of the Problem of Homelessness</h3>
+              <p class="pa-4"></p>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+        <v-window-item value="services">
+          <v-card>
+            <v-card-text>
+              <h3 class="pa-2">Types of Services</h3>
+              <h4 class="pa-4">Navigational services</h4>
+              <h4 class="pa-4">Temporary and transitional shelter</h4>
+              <h4 class="pa-4">Permanent housing assistance</h4>
+              <h4 class="pa-4">Food assistance</h4>
+              <h4 class="pa-4">Mental health and emotional support</h4>
+              <h4 class="pa-4">Medical treatment</h4>
+              <h4 class="pa-4">Job placement and training opportunities</h4>
+              <h4 class="pa-4">Substance abuse and addiction recovery</h4>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+        <v-window-item value="grantspass">
+          <h3 class="pa-2">Homelessness in Grants Pass</h3>
+          <h4 class="pa-4">The Court Injuction</h4>
+          <h4 class="pa-4">Relevant Ordinances</h4>
+          <h4 class="pa-4">Programs</h4>
+          <h4 class="pa-4">Other Actions</h4>
+        </v-window-item>
+      </v-window>
+    </v-card-text>
   </template>
   
   <script setup>
-    //
+    import { ref } from 'vue'
+    const viewTab = ref(null)
   </script>
--- path-web/src/components/Services.vue
+++ path-web/src/components/Services.vue
@@ -1,16 +1,86 @@
 <template>
-    <v-container class="fill-height">
-      <v-responsive class="align-center text-center fill-height">
-  
-        <v-row class="d-flex align-center justify-center">
-          <v-col cols="auto">
-            Services
-          </v-col>
-        </v-row>
-      </v-responsive>
-    </v-container>
+  <v-card variant="outlined" v-for="service in allServices">
+    <v-card-item>
+      <v-card-title>
+        {{ service.name }}
+      </v-card-title>
+      <v-card-text>
+        <p class="pa-4">{{ service.description }}</p>
+        <p><b>Areas Served: </b>{{ service.locations }}</p>
+        <p><b>Address: </b>{{ service.address }}</p>
+        <p><b>Phone: </b>{{ service.phone }}</p>
+        <p><b>E-Mail: </b>{{ service.email }}</p>
+      </v-card-text>
+    </v-card-item>
+  </v-card>
   </template>
   
   <script setup>
-    //
+    import { ref } from 'vue'
+    const allServices = ref(
+      [
+        {
+          name: "Angels",
+          description: "",
+          locations: "",
+          address: "1000 7th Street, Grants Pass, OR 97526",
+          phone: "(541) 555-1234",
+          email: "xxxx@yyyy.org",
+          weburl: "https://www.service.org"
+        },
+        { 
+          name: "Josephine County Food Bank",
+          description: "Food distribution center serving community pantries throughout Josephine County.  The Food Bank is NOT itself a food pickup location for the general public.  The Food Bank provides food assistance for 18,000 residents of Josephine County. ",
+          locations: "Josephine County",
+          address: "3658 Upper River Rd, Grants Pass, OR 97526",
+          phone: "(541) 555-1234",
+          email: "info@jocofoodbank.org",
+          weburl: "https://www.jocofoodbank.org"
+        },
+        {
+          name: "Maslow Project",
+          description: "",
+          locations: "",
+          address: "1000 7th Street, Grants Pass, OR 97526",
+          phone: "(541) 555-1234",
+          email: "xxxx@yyyy.org",
+          weburl: "https://www.service.org"
+        },   
+        {
+          name: "On Track Rogue Valley",
+          description: "",
+          locations: "",
+          address: "1000 7th Street, Grants Pass, OR 97526",
+          phone: "(541) 555-1234",
+          email: "xxxx@yyyy.org",
+          weburl: "https://www.service.org"
+        },    
+        {
+          name: "Options for Southern Oregon",
+          description: "",
+          locations: "",
+          address: "1000 7th Street, Grants Pass, OR 97526",
+          phone: "(541) 555-1234",
+          email: "xxxx@yyyy.org",
+          weburl: "https://www.service.org"
+        }, 
+        {
+          name: "Safe Parking Program",
+          description: "",
+          locations: "",
+          address: "1000 7th Street, Grants Pass, OR 97526",
+          phone: "(541) 555-1234",
+          email: "xxxx@yyyy.org",
+          weburl: "https://www.service.org"
+        },
+        {
+          name: "Josephine Community Library",
+          description: "Through JosephineLINK, find up-to-date information for hundreds of community organizations — legal assistance, family services, volunteer opportunities, and more. This easy-to-use community resource directory is updated regularly with the most current contact information, hours, and locations.  Wifi hotspots, laptop checkout.",
+          locations: "Grants Pass, Illinois Valley, Williams, Wolf Creek",
+          address: "200 NW C Street, Grants Pass, OR 97526",
+          phone: "(541) 476-0571",
+          email: "",
+          weburl: "https://josephinelibrary.org"
+        },
+      ])
   </script>
