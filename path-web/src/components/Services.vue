0666cc6a36fd88a941ab787a8f4ae4306bb7eb82
Matt Rosen <wizardbluebolt@gmail.com>
2024-09-02 11:58:56 -0700

    Handle mobile devices accessing community resources page.

--- path-web/src/components/Services.vue
+++ path-web/src/components/Services.vue
@@ -1,10 +1,14 @@
 <template>
   <h2 style="text-align: center; padding: 4px;">Community Resources List</h2>
-  <embed
-    class="pdf"
-    src="documents/PATH_Community_Resources_List.pdf"
+  <object
+    type="application/pdf"
+    data="documents/PATH_Community_Resources_List.pdf"
     width="100%"
     height="100%">
+    <p>The web browser on your device does not support viewing PDF documents directly.</p>
+    <p>You can download the resource list using the following link:</p>
+    <a href="documents/PATH_Community_Resources_List.pdf">Community Resources List</a>
+  </object>
 </template>
   
   <script setup>
