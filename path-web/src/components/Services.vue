b0628e12bb2556e7f12905ff7fe06426a6e4afbb
Matt Rosen <wizardbluebolt@gmail.com>
2024-09-13 06:54:29 -0700

    Updated Community Resource List.

--- path-web/src/components/Services.vue
+++ path-web/src/components/Services.vue
@@ -2,12 +2,12 @@
   <h2 style="text-align: center; padding: 4px;">Community Resource List</h2>
   <object
     type="application/pdf"
-    data="documents/PATH_Community_Resources_List.pdf"
+    data="documents/PATH_Community_Resource_List.pdf"
     width="100%"
     height="100%">
     <p class="ml-4">The web browser on your device does not support viewing PDF documents directly.</p>
     <p class="ml-4">You can download the resource list using the following link:</p>
-    <a href="documents/PATH_Community_Resources_List.pdf" class="ml-4">Community Resource List</a>
+    <a href="documents/PATH_Community_Resource_List.pdf" class="ml-4">Community Resource List</a>
   </object>
 </template>
   
