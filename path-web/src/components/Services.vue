a158189e4f098eed28c8301070eab7d6deb8a84a
Matt Rosen <wizardbluebolt@gmail.com>
2025-09-26 11:56:20 -0700

    Reordered alt text for CRL.

--- path-web/src/components/Services.vue
+++ path-web/src/components/Services.vue
@@ -1,5 +1,8 @@
 <template>
   <h2 style="text-align: center; padding: 4px;">Community Resource List</h2>
+  <p class="ml-4 mt-4">If the PDF document does not display correctly or with all pages you can download the 
+    document using the following link (you may need to press and hold and open the link in a new tab):</p>
+  <p><a href="documents/PATH_Community_Resource_List.pdf" class="ml-4">Community Resource List</a></p>
   <object
     type="application/pdf"
     data="documents/PATH_Community_Resource_List.pdf"
@@ -7,9 +10,6 @@
     height="80%">
     <p class="ml-4">The web browser on your device does not support viewing PDF documents directly.</p>
   </object>
-  <p class="ml-4 mt-4">If the PDF document does not display correctly or with all pages you can download the 
-    document using the following link (you may need to press and hold and open the link in a new tab):</p>
-  <p><a href="documents/PATH_Community_Resource_List.pdf" class="ml-4">Community Resource List</a></p>
 </template>
   
   <script setup>
